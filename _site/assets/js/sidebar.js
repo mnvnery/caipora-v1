4fdd905a7e098d332a5cc7b9c4fa3fbdf1353539
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2021-05-05 17:44:23 +0100

    changes

---
+++ _site/assets/js/sidebar.js
@@ -0,0 +1,10 @@
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    $(".menu").toggleClass("sidenav-open");
+    $(".menu-links").toggleClass("menu-links-open");
+    // Do something else, like open/close menu
+});
