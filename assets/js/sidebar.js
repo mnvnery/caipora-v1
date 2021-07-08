563fbc9d659d658107cca8009b28b0de5293b2f9
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2021-07-08 17:31:42 +0100

    setup

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,10 +1,40 @@
 // Look for .hamburger
-var hamburger = document.querySelector(".hamburger");
-// On click
-hamburger.addEventListener("click", function() {
-    // Toggle class "is-active"
-    hamburger.classList.toggle("is-active");
-    $(".menu").toggleClass("sidenav-open");
-    $(".menu-links").toggleClass("menu-links-open");
-    // Do something else, like open/close menu
-});
+const mediaQuery = window.matchMedia('(max-width: 600px)')
+
+
+    var hamburger = document.querySelector(".hamburger");
+    // On click
+    hamburger.addEventListener("click", function() {
+        // Toggle class "is-active"
+        hamburger.classList.toggle("is-active");
+        $(".menu").toggleClass("sidenav-open");
+        $(".menu-links").toggleClass("menu-links-open");
+        $(".socials").toggleClass("socials-open");
+        if (mediaQuery.matches) {
+            $(".page-title").toggle();
+            // Do something else, like open/close menu
+        }
+    });
+
+
+
+
+    $('.faq-question:even').addClass('green');
+    $('.faq-answer:even').addClass('green');
+    $('.arrow-faq').click(function() {
+        jQuery(this).parent().next().toggle();
+        $(this).toggleClass('ri-arrow-up-s-line');
+    });
+
+
+
+    if (mediaQuery.matches) {
+        $('.team-img').click(function(){
+            $(this).next().toggle()
+        })
+    }
+    else {
+        $('.team-img').hover(function(){
+            $(this).next().toggle()
+        })
+    }
